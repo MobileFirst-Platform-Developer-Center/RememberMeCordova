67ee4f322e28adfd0edb9e5e067150929cf9efca
Lior Burg <liorbu@il.ibm.com>
2016-02-25 13:47:32 +0200

    Added license headers and fixed the widget id value

--- www/js/UserLoginChallengeHandler.js
+++ www/js/UserLoginChallengeHandler.js
@@ -1,15 +1,30 @@
+/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 var UserLoginChallengeHandler = function() {
     var isChallenged = false;
     var securityCheckName = 'UserLogin';
     var userLoginChallengeHandler = WL.Client.createWLChallengeHandler(securityCheckName);
-    
+
     document.getElementById("login").addEventListener("click", login);
     document.getElementById("logout").addEventListener("click", logout);
-    
+
     userLoginChallengeHandler.securityCheckName = securityCheckName;
-        
+
     userLoginChallengeHandler.handleChallenge = function(challenge) {
-        WL.Logger.debug("handleChallenge");       
+        WL.Logger.debug("handleChallenge");
         showLoginDiv();
         isChallenged = true;
         var statusMsg = "Remaining Attempts: " + challenge.remainingAttempts;
@@ -18,7 +33,7 @@ var UserLoginChallengeHandler = function() {
         }
         document.getElementById("statusMsg").innerHTML = statusMsg;
     }
-    
+
     userLoginChallengeHandler.processSuccess = function(data) {
         WL.Logger.debug("processSuccess");
         isChallenged = false;
@@ -28,7 +43,7 @@ var UserLoginChallengeHandler = function() {
         document.getElementById("helloUser").innerHTML = "Hello " + data.user.displayName;
         showProtectedDiv();
     }
-    
+
     userLoginChallengeHandler.handleFailure = function(error) {
         WL.Logger.debug("handleFailure: " + error.failure);
         isChallenged = false;
@@ -38,7 +53,7 @@ var UserLoginChallengeHandler = function() {
             alert("Failed to login.");
         }
     }
-    
+
     function login() {
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
@@ -48,7 +63,7 @@ var UserLoginChallengeHandler = function() {
             return;
         }
         if (isChallenged){
-            userLoginChallengeHandler.submitChallengeAnswer({'username':username, 'password':password, rememberMe: rememberMeState});    
+            userLoginChallengeHandler.submitChallengeAnswer({'username':username, 'password':password, rememberMe: rememberMeState});
         } else {
             WLAuthorizationManager.login(securityCheckName,{'username':username, 'password':password, rememberMe: rememberMeState}).then(
                 function () {
@@ -59,7 +74,7 @@ var UserLoginChallengeHandler = function() {
                 });
         }
     }
-    
+
     function logout() {
     WLAuthorizationManager.logout(securityCheckName).then(
         function () {
@@ -70,7 +85,7 @@ var UserLoginChallengeHandler = function() {
             WL.Logger.debug("logout onFailure: " + JSON.stringify(response));
         });
     }
-    
+
     return userLoginChallengeHandler;
-    
+
 };
--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,19 @@
-var Messages = {
+/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+ar Messages = {
     // Add here your messages for the default language.
     // Generate a similar file with a language suffix containing the translated messages.
     // key1 : message1,
@@ -17,7 +32,7 @@ function wlCommonInit(){
     WLAuthorizationManager.obtainAccessToken(userLoginChallengeHandler.securityCheckName).then(
         function (accessToken) {
             WL.Logger.debug("obtainAccessToken onSuccess");
-            showProtectedDiv(); 
+            showProtectedDiv();
         },
         function (response) {
             WL.Logger.debug("obtainAccessToken onFailure: " + JSON.stringify(response));
@@ -26,13 +41,13 @@ function wlCommonInit(){
 }
 
 function showLoginDiv() {
-    document.getElementById('protectedDiv').style.display = 'none'; 
+    document.getElementById('protectedDiv').style.display = 'none';
     document.getElementById('statusMsg').innerHTML = "";
     document.getElementById('loginDiv').style.display = 'block';
 }
 
 function showProtectedDiv() {
-    document.getElementById('loginDiv').style.display = 'none'; 
+    document.getElementById('loginDiv').style.display = 'none';
     document.getElementById('resultLabel').innerHTML = "";
     document.getElementById('protectedDiv').style.display = 'block';
 }
@@ -49,4 +64,3 @@ function getBalance () {
             document.getElementById("resultLabel").innerHTML = "Failed to get balance.";
         });
 }
-
